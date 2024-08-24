c367b20ddfda9fcec8ebd0c24f3a1b0353a17a99
권예림 <124475716+yerim07@users.noreply.github.com>
2024-08-24 14:57:01 +0900

    Post :: error
    에러 있어요

--- axioss/src/App.jsx
+++ axioss/src/App.jsx
@@ -1,20 +1,76 @@
+// Get
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// const App = () => {
+//   const [todos, setTodos] = useState();
+
+//   const fetchTodos = async () => {
+//     const { data } = await axios.get("https://reqres.in/api/users?page=2");
+//     setTodos(data);
+//   };
+
+//   useEffect(() => {
+//     fetchTodos();
+//   }, []);
+
+//   console.log(todos);
+//   return <div>App</div>;
+// };
+
+// export default App;
+
+//Post
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [todos, setTodos] = useState();
+  const [todo, setTodo] = useState({
+    title: "",
+  });
+
+  const [todos, setTodos] = useState(null);
 
   const fetchTodos = async () => {
-    const { data } = await axios.get("https://reqres.in/api/users?page=2");
+    const { data } = await axios.get("https://reqres.in/api/users");
     setTodos(data);
+  };
+
+  const onSubmitHandler = (todo) => {
+    axios.post("https://reqres.in/api/users", todo);
   };
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
-  console.log(todos);
-  return <div>App</div>;
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmitHandler(todo);
+        }}
+      >
+        <input
+          type="text"
+          onChange={(ev) => {
+            const { value } = ev.target;
+            setTodo({
+              ...todo,
+              title: value,
+            });
+          }}
+        />
+        <button>추가하기</button>
+      </form>
+      <div>
+        {todos?.map((todo) => (
+          <div key={todo.id}>{todo.title}</div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
